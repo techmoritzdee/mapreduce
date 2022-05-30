bf2e0a7e8cbae8102da9899da889037e4ab58539
techmoritzdee <83080241+techmoritzdee@users.noreply.github.com>
2022-05-30 23:45:48 +0200

    Delete 1mapper.py

--- 1mapper.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-for line in sys.stdin:
-    data = line.strip().split("\t")
-
-    date, time, item, category, sales, payment = data
-    
-    sys.stdout.write("{0}\t{1}\n".format(category, sales))
